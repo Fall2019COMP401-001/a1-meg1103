cbfc3a80e0a0754dabbc606189951a3c08bac3e8
megan <megan@152.23.118.181>
2019-08-30 13:05:05 -0400

    attempt 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,35 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Reads in count of names and stored in name_count
+		
+		int name_count = scan.nextInt();
+		
+		for (int i = 0; i < name_count; i++ ) {
+			
+			String first_name = scan.next(); 
+			
+			char[] first_name_arr = first_name.toCharArray();
+			
+			String first_init = first_name_arr[1] + ".";
+			
+			String last_name = scan.next();
+			
+			double total = 0; 
+			
+			int food_count = scan.nextInt();
+			
+			for (int u = 0; u < food_count; u++ ) {
+				int amt = scan.nextInt();
+				double price = scan.nextDouble() * amt;
+				total += price; 
+				
+			}
+			System.out.println(first_init + " " + last_name + ": " + total);
+			
+		}
+		
+		
+		
 	}
 }
